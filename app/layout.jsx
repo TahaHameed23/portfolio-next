19e13e2e250e673cdf84f5545e8f473b3f20bf56
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2024-08-22 22:30:11 +0530

    add:ico

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,6 +8,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
+        <link rel="apple-touch-icon" sizes="180x180" href="./ico/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="./ico/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="./ico/favicon-16x16.png" />
+        <link rel="manifest" href="./ico/site.webmanifest"></link>
       </head>
       <body>{children}</body>
     </html>
