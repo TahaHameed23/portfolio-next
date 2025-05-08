9671abf983cc17ff1dd260903212f690d8dd0fd5
Taha Hameed <tahahameed29@gmail.com>
2025-05-09 01:44:46 +0530

    Update Contact.jsx

--- app/about/components/Contact.jsx
+++ app/about/components/Contact.jsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <div className="flex flex-col w-fit">
         <div className="link"><a href={"https://github.com/tahahameed23"} target="_blank" rel="noopener noreferrer" >Github</a></div>
-        <div className="link"><a href={""} target="_blank" rel="noopener noreferrer">Resume</a></div>
+        <div className="link"><a href={"https://drive.google.com/file/d/1oORen-5f8zH31BDbzz7birlWIIsTRAz8/view?usp=drivesdk"} target="_blank" rel="noopener noreferrer">Resume</a></div>
     </div>
   )
 }
